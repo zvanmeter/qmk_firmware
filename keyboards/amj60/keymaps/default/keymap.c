3b836af752d3ab8cd7df4b450b3c39f59eb5cf02
skullY <skullydazed@gmail.com>
2017-10-15 14:21:38 -0700

    remove unneeded comment

--- keyboards/amj60/keymaps/default/keymap.c
+++ keyboards/amj60/keymaps/default/keymap.c
@@ -1,6 +1,3 @@
-// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
-// this is the style you want to emulate.
-
 #include "amj60.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
